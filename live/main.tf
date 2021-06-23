feb983067e86943997ece41f0c88a59014cc7d25
Silvio Silva <smsilva@gmail.com>
2021-06-23 13:25:13 -0300

    Teste

--- live/main.tf
+++ live/main.tf
@@ -1,1 +1,13 @@
-/* empty file - Terragrunt will generate all resources */
+variable "name" {
+  default = "silvios"
+}
+
+resource "null_resource" "creator" {
+  triggers = {
+    name = "prefix-${var.name}-sufix"
+  }
+}
+
+output "id" {
+  value = null_resource.creator.triggers.name
+}
